b4eeb96322c7deb63018481103ec185ae5ec70bc
Hashem Khodor <hashemkhoder1@gmail.com>
2024-05-10 15:52:41 +0300

    Changes

--- FullyRetroactiveDSU.cpp
+++ FullyRetroactiveDSU.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-typedef long double ld;
+typedef long long ld;
 
 #define fastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define dbg(x) cerr << #x << " = " << x << endl;
 
 const ld DEFAULT = -1;
-const int mxN = 2e5 + 2;
+const int mxN = 4e5 + 2;
 
 struct Node
 {
@@ -25,69 +23,69 @@ protected:
     SplayTree(int n) : T(n + 1) {}
 
     // Push lazy propagation, to be called before exposeing children
-    void push_children(int x)
+    void push_to_children(int u)
     {
-        if (!x || !T[x].flip)
+        if (!u || !T[u].flip)
             return;
-        int l = T[x].child[0], r = T[x].child[1];
+        int l = T[u].child[0], r = T[u].child[1];
         T[l].flip ^= 1, T[r].flip ^= 1;
-        swap(T[x].child[0], T[x].child[1]);
-        T[x].flip = 0;
+        swap(T[u].child[0], T[u].child[1]);
+        T[u].flip = 0;
     }
 
     // Update path value of node x
-    void update_node(int x)
+    void update_node_from_children(int u)
     {
-        int l = T[x].child[0], r = T[x].child[1];
-        push_children(l), push_children(r);
-        T[x].path = max({T[l].path, T[r].path, T[x].val});
+        int l = T[u].child[0], r = T[u].child[1];
+        push_to_children(l), push_to_children(r);
+        T[u].path = max({T[l].path, T[r].path, T[u].val});
     }
 
     // Set child of x to y at direction d
-    void set_child(int x, int d, int y)
+    void set_child(int u, int side, int v)
     {
-        T[x].child[d] = y;
-        T[y].parent = x;
-        update_node(x);
+        T[u].child[side] = v;
+        T[v].parent = u;
+        update_node_from_children(u);
     }
 
     // find side of x relative to its parent
-    int side(int x)
+    int side(int u)
     {
-        int p = T[x].parent;
-        if (!p)
+        int parent = T[u].parent;
+        if (!parent)
             return -1;
-        if (T[p].child[0] == x)
+        if (T[parent].child[0] == u)
             return 0;
-        if (T[p].child[1] == x)
+        if (T[parent].child[1] == u)
             return 1;
         return -1;
     }
 
     // Rotate x with its parent
-    void rotate(int x)
+    void rotate(int u)
     {
-        int y = T[x].parent, g = T[y].parent, dx = side(x), dy = side(y);
-        set_child(y, dx, T[x].child[!dx]);
-        set_child(x, !dx, y);
-        if (dy != -1)
-            set_child(g, dy, x);
+        int parent = T[u].parent, grandParent = T[parent].parent, x_side = side(u), y_side = side(parent);
+        set_child(parent, x_side, T[u].child[!x_side]);
+        set_child(u, !x_side, parent);
+        if (y_side != -1)
+            set_child(grandParent, y_side, u);
         else
-            T[x].parent = g;
+            T[u].parent = grandParent;
     }
 
     // splay x to root
-    void splay(int x)
+    void splay(int u)
     {
-        push_children(x);
-        while (side(x) != -1)
+        push_to_children(u);
+        while (side(u) != -1)
         {
-            int p = T[x].parent, g = T[p].parent;
-            push_children(g), push_children(p), push_children(x);
-            int dx = side(x), dy = side(p);
-            if (dy != -1)
-                rotate((dx == dy) ? p : x);
-            rotate(x);
+            int parent = T[u].parent, g = T[parent].parent;
+            push_to_children(g), push_to_children(parent), push_to_children(u);
+            int x_side = side(u), parent_side = side(parent);
+            if (parent_side != -1)
+                rotate((x_side == parent_side) ? parent : u);
+            rotate(u);
         }
     }
 };
@@ -99,27 +97,27 @@ protected:
     int edges[mxN] = {0};
 
     // Make the path from the root node to x a preferred path
-    int expose(int x)
+    int expose(int u)
     {
-        int last = 0, y = x;
+        int last = 0, y = u;
         while (y)
         {
             splay(y);
             T[y].child[1] = last;
-            push_children(y);
+            update_node_from_children(y);
             last = y;
             y = T[y].parent;
         }
-        splay(x);
+        splay(u);
         return last;
     }
 
     // makes x the root of the represented tree
-    void evert(int x)
+    void evert(int u)
     {
-        expose(x);
-        T[x].flip ^= 1;
-        push_children(x);
+        expose(u);
+        T[u].flip ^= 1;
+        push_to_children(u);
     }
 
     // add the Edge between x and y
@@ -128,7 +126,7 @@ protected:
         evert(u);
         expose(v);
         T[u].parent = v;
-        push_children(v);
+        update_node_from_children(v);
     }
 
     // remove the Edge between x and y
@@ -137,10 +135,10 @@ protected:
         evert(u);
         expose(v);
         T[v].child[0] = T[u].parent = 0;
-        push_children(v);
+        update_node_from_children(v);
     }
 
-public:
+protected:
     Link_Cut_Tree(int n) : SplayTree(n) {}
 
     // remove the Edge between x and y
@@ -148,6 +146,8 @@ public:
     {
         if (u > v)
             swap(u, v);
+        if (find(u) != find(v))
+            return;
         cut(u, edges[u]);
         edges[u] = edges[v] = 0;
     }
@@ -155,8 +155,12 @@ public:
     // returns the maximum value on the path from u to v
     ld PathQuery(int u, int v)
     {
+        if (find(u) != find(v))
+            return -1;
         evert(u);
         expose(v);
+        if (u == v)
+            return 0;
         return T[v].path;
     }
 
@@ -165,7 +169,7 @@ public:
     {
         expose(u);
         T[u].val = v;
-        update_node(u);
+        update_node_from_children(u);
     }
 
     // returns the root of the represented tree that contains u
@@ -181,6 +185,8 @@ public:
     {
         if (u > v)
             swap(u, v);
+        if (find(u) == find(v))
+            return;
         link(u, nodeCount);
         link(v, nodeCount);
         Update(nodeCount, w);
@@ -195,7 +201,7 @@ struct Fully_Retroactive_DSU : Link_Cut_Tree
 
     void UNION(int u, int v, ld time)
     {
-        assert(find(u) != find(v));
+        if (find(u) == find(v)) return;
         event.insert({time, {u, v}});
         Link(u, v, time);
     }
@@ -204,7 +210,8 @@ struct Fully_Retroactive_DSU : Link_Cut_Tree
     {
         int u = event.lower_bound({time, {0, 0}})->second.first;
         int v = event.lower_bound({time, {0, 0}})->second.second;
-        assert(find(u) == find(v));
+        if(event.lower_bound({time, {0, 0}}) == event.end()) return;
+        if(find(u) != find(v)) return;
         Cut(u, v);
         event.erase(event.find({time, {u, v}}));
     }
@@ -269,9 +276,70 @@ void solve()
     }
 }
 
+void gen(int num_ops) {
+    const int numNodes = 1e5;
+    Fully_Retroactive_DSU DSU;
+    stack<ld> tm;
+    ld cnt = 0;
+    for (int i = 0; i < num_ops; i++) {
+        int op = rand() % 3;
+        if (op == 0) {
+            int u = rand() % numNodes + 1, v = rand() % numNodes + 1;
+            ld time = ++cnt;
+            tm.push(time);
+            DSU.UNION(u, v, time);
+        } else if (op == 1) {
+            if(tm.empty()) continue;
+            long double time = tm.top(); tm.pop();
+            DSU.ERASE(time);
+        } else {
+            int u = rand() % numNodes + 1, v = rand() % numNodes + 1;
+            long double time = rand() % (cnt + 1);
+            DSU.QUERY(u, v, time);
+        }
+    }
+}
+
+void test_n_logn(int sz) {
+    vector<int> nums(sz); 
+    for(int i = 0; i < sz; i++) nums[i] = rand() % mxN;
+    shuffle(nums.begin(), nums.end(), mt19937{random_device{}()});
+    sort(nums.begin(), nums.end());
+    shuffle(nums.begin(), nums.end(), mt19937{random_device{}()});
+    sort(nums.begin(), nums.end());
+}
+
+
+#include <chrono>
+vector<long double> time_taken;
+vector<int> num_ops = {120000};
+void stress_test() {
+    for (int i = 0; i < num_ops.size(); i++) {
+        auto start = chrono::high_resolution_clock::now();
+        gen(num_ops[i]);
+        auto end = chrono::high_resolution_clock::now();
+        time_taken.push_back(chrono::duration_cast<chrono::milliseconds>(end - start).count());
+        cout << "Time taken for " << num_ops[i] << " operations: " << time_taken[i] << "ms\n";
+    }
+}
+
+void test() {
+    for(int i = 0; i < num_ops.size(); i++) {
+        auto start = chrono::high_resolution_clock::now();
+        test_n_logn(num_ops[i]);
+        auto end = chrono::high_resolution_clock::now();
+        time_taken.push_back(chrono::duration_cast<chrono::milliseconds>(end - start).count());
+    }
+    for (int i = 0; i < num_ops.size(); i++) {
+        cout << "Time taken for " << num_ops[i] << " n*log(n) operations: " << time_taken[i] << "ms\n";
+    }
+}
+
 int main()
 {
-    // fastIO;
-    solve();
+    freopen("output.txt", "w", stdout);
+    srand(10);
+    // test();
+    // stress_test();
     return 0;
 }
